31d4df06fb2f18254fe128a885a6257ead80c2fb
Duncan Walker <duncfw@gmail.com>
2015-09-17 12:34:04 +0100

    Add remain GET endpoints:
    * getAO
    * candidate channels
    * custom attributes
    * twitter authorize and create friendship
    * add xmpp contact

--- test/Nuntium.test.js
+++ test/Nuntium.test.js
@@ -8,25 +8,26 @@ describe('Nuntium', function() {
 		describe('api', function () {
 				var callback, api, server;
 				before(function() {
-						api = new Nuntium("account","application","password");
+						api = new Nuntium("account_name","application_name","password");
 				});
 				beforeEach(function() {
 						server = sinon.mock(rest);
 						callback = sinon.spy();
 				});
 				afterEach(function() {
-						server.restore();
+					// TODO: this fails on callbacks that throw exceptions	assert(callback.calledOnce);
 						callback.reset();
+						server.restore();
 				});
 				it('gets countries', function () {
 						expectGet('/api/countries.json',[{"name": "Argentina", "iso2": "ar"}]);
 						api.getCountries(callback);
-						assert(callback.withArgs([{"name": "Argentina", "iso2": "ar"}]).calledOnce);
+						sinon.assert.calledWith(callback,[{"name": "Argentina", "iso2": "ar"}]);
 				});
 				it('gets country', function () {
 						expectGet('/api/countries/ar.json',{"name": "Argentina", "iso2": "ar"});
 						api.getCountry('ar',callback);
-						assert(callback.withArgs({"name": "Argentina", "iso2": "ar"}).calledOnce);
+						sinon.assert.calledWith(callback,{"name": "Argentina", "iso2": "ar"});
 				});
 				it('requires and id to get a country',function() {
 						assert.throws(api.getCountry(null,callback));
@@ -34,17 +35,17 @@ describe('Nuntium', function() {
 				it('gets carriers', function () {
 						expectGet('/api/carriers.json',[{"name": "Argentina", "iso2": "ar"}]);
 						api.getCarriers(callback);
-						assert(callback.withArgs([{"name": "Argentina", "iso2": "ar"}]).calledOnce);
+						sinon.assert.calledWith(callback,[{"name": "Argentina", "iso2": "ar"}]);
 				});
 				it('gets carriers for a country',function() {
 						expectGet('/api/carriers.json?country_id=ar',[{"name": "Argentina", "iso2": "ar"}]);
 						api.getCarriers('ar',callback);
-						assert(callback.withArgs([{"name": "Argentina", "iso2": "ar"}]).calledOnce);
+						sinon.assert.calledWith(callback,[{"name": "Argentina", "iso2": "ar"}]);
 				});
 				it('gets carrier',function() {
 						expectGet('/api/carriers/ar.json',{"name": "Argentina", "iso2": "ar"});
 						api.getCarrier('ar',callback);
-						assert(callback.withArgs({"name": "Argentina", "iso2": "ar"}).calledOnce);
+						sinon.assert.calledWith(callback,{"name": "Argentina", "iso2": "ar"});
 				});
 				it('requires and id to get a carrier',function() {
 						assert.throws(api.getCarrier(null,callback));
@@ -52,24 +53,53 @@ describe('Nuntium', function() {
 				it('gets channels',function() {
 						expectGet('/api/channels.json',[{"name": "Argentina", "configuration": [{"name": "foo", "value": "bar"}]}]);
 						api.getChannels(callback);
-						assert(callback.withArgs([{"name": "Argentina", "configuration": [{"name": "foo", "value": "bar"}]}]).calledOnce);
+						sinon.assert.calledWith(callback,[{"name": "Argentina", "configuration": [{"name": "foo", "value": "bar"}]}]);
 				});
 				it('gets channel',function() {
 						expectGet('/api/channels/Argentina.json',{"name": "Argentina", "configuration": [{"name": "foo", "value": "bar"}]});
 						api.getChannel('Argentina',callback);
-						assert(callback.withArgs({"name": "Argentina", "configuration": [{"name": "foo", "value": "bar"}]}).calledOnce);
+						sinon.assert.calledWith(callback,{"name": "Argentina", "configuration": [{"name": "foo", "value": "bar"}]});
 				});
 				it('updates channel');
 				it('deletes channel');
-				it('gets candidate channels for ao');
+				it('gets candidate channels for ao', function() {
+						expectGet('/api/candidate/channels.json?from=sms%3A%2F%2F1234&body=Hello',[{"name":"Argentina","configuration":[{"value":"bar","name":"foo"}]}]);
+
+						api.getCandidateChannelsForAO({'from':'sms://1234','body':'Hello'},callback);
+						sinon.assert.calledWith(callback,[{"name":"Argentina","configuration":[{"value":"bar","name":"foo"}]}]);
+						assert(callback.calledOnce);
+				});
 				it('sends single ao');
 				it('sends many aos');
-				it('gets ao');
-				it('gets custom attributes');
+				it('gets ao',function() {
+						expectGet('/account_name/application_name/get_ao.json?token=foo',[{"name": "Argentina", "iso2": "ar"}]);
+						api.getAO('foo',callback);
+						sinon.assert.calledWith(callback,[{"name": "Argentina", "iso2": "ar"}]);
+						assert(callback.calledOnce);
+				});
+				it('gets custom attributes',function() {
+						expectGet('/api/custom_attributes?address=foo',[{"name": "Argentina", "iso2": "ar"}]);
+
+						api.getCustomAttributes('foo',callback);
+						sinon.assert.calledWith(callback,[{"name": "Argentina", "iso2": "ar"}]);
+						assert(callback.calledOnce);
+				});
 				it('sets custom attributes');
-				it('creates twitter friendship');
-				it('authorizes twitter channel');
-				it('adds xmpp contact');
+				it('creates twitter friendship',function() {
+						expectGet('/api/channels/twit/twitter/friendships/create?user=foo&follow=true');
+						api.createTwitterFriendship({'channel':'twit','user':'foo','follow':true});
+				});
+				it('authorizes twitter channel',function() {
+						expectGet('/api/channels/twit/twitter/authorize?callback=foo','http://bar');
+
+						api.twitterAuthorize({'channel':'twit','callback':'foo'},callback);
+						sinon.assert.calledWith(callback,'http://bar');
+						assert(callback.calledOnce);
+				});
+				it('adds xmpp contact',function() {
+						expectGet('/api/channels/chan/xmpp/add_contact?id=foo%40bar.com');
+						api.xmppAddContact({'channel':'chan','id':'foo@bar.com'});
+				});
 
 				function expectGet(url,data) {
 						var request = new Request('', {});
